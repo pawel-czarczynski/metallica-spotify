ed638f744ebdac5a9e1418b2a49f3c941f225dc4
Pawel Czarczynski <pawel.czarczynski@live.co.uk>
2023-06-13 22:37:14 +0100

    z

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({albums}) {
   return (
     <>
       <Head>
-        <title>Metallica by Pawel CZarczynski</title>
+        <title>Metallica by Pawel Czarczynski</title>
         <meta name="description" content="Created by Pawel Czarczynski" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
